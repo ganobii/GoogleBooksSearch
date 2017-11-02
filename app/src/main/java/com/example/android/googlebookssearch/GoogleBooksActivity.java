bde0ce5ae09dc169041e34c706447da6f3242c33
ganobii <gnmills@gmail.com>
2017-11-02 16:48:10 -0300

    first commit

---
+++ app/src/main/java/com/example/android/googlebookssearch/GoogleBooksActivity.java
@@ -0,0 +1,13 @@
+package com.example.android.googlebookssearch;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GoogleBooksActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_google_books);
+    }
+}
